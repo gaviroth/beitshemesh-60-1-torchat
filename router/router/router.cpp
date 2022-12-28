a2dbfc1a42b7ed58a01cda6cab34a472a27bac2c
Dor Lev <dorlev2508@gmail.com>
2022-12-28 19:19:11 +0000

    router.cpp recup

--- router/router/router.cpp
+++ router/router/router.cpp
@@ -1,15 +1,19 @@
+// router.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
 #pragma comment(lib, "ws2_32.lib")
 
 #include <thread>
 #include <vector>
+#include <map>
 #include <time.h> 
 #include <iostream>
 #include <stdlib.h>/* srand, rand */
 #include "WSAInitializer.h"
-
-
+#include <string>
 #define PORT 2345
-static const char* port[5]={"1235", "1236", "1237", "1238", "1239"};//the ports of the middle servers
+
+static std::map<std::string /*port*/,std::string /*public key*/> ms_ports;
+static std::string spk;
 SOCKET bindAndListen()
 {
 	struct sockaddr_in sa = { 0 };
@@ -30,43 +34,102 @@ void handleNewClient(SOCKET a)
 {
 	char m[1024];
 	recv(a, m, strlen(m), 0);
-	srand(time(NULL));
-	int r1= rand() % 4 + 0;
-	int r2 = rand() % 4 + 0;
-	int r3 = rand() % 4 + 0;
-	while (r2==r1)
+	if (m[0] == 's'&&m[1]=='p' && m[2] == 'k')
 	{
-		r2 = rand() % 4 + 0;
+		std::string port1 = "1234";
+		std::string spk1 = std::to_string(m[3]) +m[3]+ m[4] + m[5] + m[6] + m[7] + m[8];
+		spk1 = m[3] + m[4] + m[5] + m[6] + m[7] + m[8];
+		ms_ports.insert({ port1,spk1 });
 	}
-	while (r1==r3||r2==r3 )
+	if (m[0] == 'p' && m[1] == 'k')
 	{
-		r3 = rand() % 4 + 0;
-	}//r1,r2 and r3 are thrre diffrent random numbers betwwen 0 to 4;
-	if (m[0] == 's')
-	{
-		std::string qw=port[r1];
-		std::string qw1 = port[r2];
-		std::string qw2 = port[r3];
-		std::string q = qw+qw1+qw2;
-		std::cout << q;
-		send(a, q.c_str(), strlen(q.c_str()), 0);
+		std::string  port1 = std::to_string(m[3]) + m[3] + m[4] + m[5] + m[6] + m[7] + m[8];
+		port1 = m[2] + m[3] + m[4] + m[5];
+		std::string spk1 = std::to_string(m[3]) + m[3] + m[4] + m[5] + m[6] + m[7] + m[8];
+		spk1 = m[3] + m[4] + m[5] + m[6] + m[7] + m[8];
+		ms_ports.insert({ port1,spk1 });
 
-	}// making a mmesage that will include a path in case that the server asks for a path and than he wiil need to add his target in the end
+	}
 	else
 	{
-		std::string qw = port[r1];
-		std::string qw1 = port[r2];
-		std::string qw2 = port[r3];
-		std::string qw3 = "1234";//the server port....
+		srand(time(NULL));
+		int r1 = rand() % ms_ports.size() + 0;
+		int r2 = rand() % ms_ports.size() + 0;
+		int r3 = rand() % ms_ports.size() + 0;
+		while (r2 == r1)
+		{
+			r2 = rand() % ms_ports.size() + 0;
+		}
+		while (r1 == r3 || r2 == r3)
+		{
+			r3 = rand() % ms_ports.size() + 0;
+		}//r1,r2 and r3 are thrre diffrent random numbers betwwen 0 to 4;
+		if (m[0] == 's')
+		{
+			std::string qw;
+			std::string qw1;
+			std::string qw2;
+			int i = 0;
+			for (auto it = ms_ports.begin(); it != ms_ports.end(); it++)
+			{
+				if (it->second.length() == 2)
+					it->second == "0000" + it->second;
+				if (it->second.length() == 3)
+					it->second == "000" + it->second;
+				if (it->second.length() == 4)
+					it->second == "00" + it->second;
+				if (it->second.length() == 5)
+					it->second == "0" + it->second;
+				if (i == r1)
 
-		std::string q = qw3+qw + qw1 + qw2;
-		std::cout << q; 
-		send(a, q.c_str(), strlen(q.c_str()), 0);
-	}// making a mmesage that will include a path in case that the client asks for a path and the server is the last target....
+					qw = it->first + it->second;
+				if (i == r2)
+					qw1 = it->first + it->second;
+				if (i == r3)
+					qw2 = it->first + it->second;
+				i++;
+			}
+			
+			std::string q = qw + qw1 + qw2;
+			std::cout << q;
+			send(a, q.c_str(), strlen(q.c_str()), 0);
+
+		}// making a mmesage that will include a path in case that the server asks for a path and than he wiil need to add his target in the end
+		else if (m[0] == 'c')
+		{
+			std::string qw;
+			std::string qw1;
+			std::string qw2;
+			int i = 0;
+			for (auto it = ms_ports.begin(); it != ms_ports.end(); it++)
+			{
+				if (it->second.length() == 2)
+					it->second == "0000" + it->second;
+				if (it->second.length() == 3)
+					it->second == "000" + it->second;
+				if (it->second.length() == 4)
+					it->second == "00" + it->second;
+				if (it->second.length() == 5)
+					it->second == "0" + it->second;
+				if (i == r1)
+					
+					qw = it->first+it->second ;
+				if (i == r2)
+					qw1 = it->first+it->second;
+				if (i == r3)
+					qw2 = it->first+it->second;
+				i++;
+			}
+			std::string qw3 = "1234"+spk;//the server port....
+
+			std::string q = qw3 + qw + qw1 + qw2;
+			std::cout << q;
+			send(a, q.c_str(), strlen(q.c_str()), 0);
+		}// making a mmesage that will include a path in case that the client asks for a path and the server is the last target....
 
 
-	closesocket(a);
-
+		closesocket(a);
+	}
 }
 void startHandleRequests()
 {
