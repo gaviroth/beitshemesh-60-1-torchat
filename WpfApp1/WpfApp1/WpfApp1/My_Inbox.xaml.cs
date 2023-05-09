b798700a2169d0aaeb71d4443ac6c53497c35f55
Dor Lev <dorlev2508@gmail.com>
2023-05-09 11:13:35 +0000

    Update My_Inbox.xaml.cs

--- WpfApp1/WpfApp1/WpfApp1/My_Inbox.xaml.cs
+++ WpfApp1/WpfApp1/WpfApp1/My_Inbox.xaml.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -35,6 +35,7 @@ namespace WpfApp1
         {
             my_conn = conn;
             my_username = username;
+            Username.Text=username
             InitializeComponent();
             SQLiteCommand command = new SQLiteCommand("SELECT * FROM Inbox", my_conn);
 
